4f580d6df93f78261ebe10f09e26bffce4b11c9c
Simlex <simlexafol@gmail.com>
2024-09-14 22:39:03 +0100

    feat: add children prop to layout

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -3,13 +3,14 @@ import Navbar from "./shared/navbar";
 import Footer from "./shared/footer";
 
 interface LayoutProps {
-    
+    children: React.ReactNode;
 }
  
-const Layout: FunctionComponent<LayoutProps> = ():ReactElement => {
+const Layout: FunctionComponent<LayoutProps> = ({ children }: { children: React.ReactNode }):ReactElement => {
     return ( 
         <>
             <Navbar />
+            {children}
             <Footer />
         </>
      );
