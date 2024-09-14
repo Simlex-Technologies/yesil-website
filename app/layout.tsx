3af8e9215acaf1c7f79f250c71f4b66ea898523e
Simlex <simlexafol@gmail.com>
2024-09-14 22:20:37 +0100

    feat: layout setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,38 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.scss";
+import Layout from "./components/Layout";
 
 const plusJakartaSans = localFont({
-  src: "./fonts/PlusJakartaSans.ttf",
-  variable: "--font-plus-jakarta-sans",
-  weight: "100 900",
+    src: "./fonts/PlusJakartaSans.ttf",
+    variable: "--font-plus-jakarta-sans",
+    weight: "100 900",
 });
 const plusJakartaSansItalic = localFont({
-  src: "./fonts/PlusJakartaSans-Italic.ttf",
-  variable: "--font-plus-jakarta-italic-sans",
-  weight: "100 900",
+    src: "./fonts/PlusJakartaSans-Italic.ttf",
+    variable: "--font-plus-jakarta-italic-sans",
+    weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: "Versil Services",
-  description: "Seamless HR Solutions and Logistics Excellence",
+    title: "Versil Services",
+    description: "Seamless HR Solutions and Logistics Excellence",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${plusJakartaSans.variable} ${plusJakartaSansItalic.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${plusJakartaSans.variable} ${plusJakartaSansItalic.variable} antialiased`}
+            >
+                <Layout>
+                    {children}
+                </Layout>
+            </body>
+        </html>
+    );
 }
