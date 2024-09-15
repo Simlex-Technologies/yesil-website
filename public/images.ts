b27b99d71612e3d68245d4f282486bdddbf988ef
Simlex <simlexafol@gmail.com>
2024-09-15 15:59:30 +0100

    feat: add images

---
+++ public/images.ts
@@ -0,0 +1,13 @@
+import logo from "./images/logo.png";
+import hero_img from "./images/hero_img.png";
+import google from "./images/google.png";
+import hr_lady from "./images/hr_lady.jpg";
+import logo_full_white from "./images/logo_full_white.png";
+
+export default {
+    logo,
+    hero_img,
+    google,
+    hr_lady,
+    logo_full_white,
+}
