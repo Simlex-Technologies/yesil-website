ac96de7f6e5bd3dad4e8d40d1914e23f2fd4929b
Simlex <simlexafol@gmail.com>
2024-09-15 15:58:58 +0100

    style: add styles and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,22 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "#01206F",
+          foreground: "#ffffff",
+          light: "#E1E9FD",
+        },
+        secondary: {
+          DEFAULT: "#C59945",
+          foreground: "#ffffff",
+          light: "#DFD1B6",
+        },
+        "dark-grey": "#202124",
+        "sky-blue": "#02395C",
+      },
+      boxShadow: {
+        nav: "0px 5px 15px 0px #00000033",
+        service: "8px 9px 26.8px 2px #0059441A",
       },
     },
   },
